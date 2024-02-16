14fdbff7489f0eb378330da046cd47bac1b38e48
onlydominik <kdominikpl@gmail.com>
2024-02-16 21:04:43 +0100

    New challenge

--- index2.js
+++ index2.js
@@ -18,8 +18,24 @@ let calculator = {
 calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
-*/
+
 //challenge 2
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+  },
+};
+ladder.up().up().down().showStep().down().showStep();
+*/
+//challenge 3
 let ladder = {
   step: 0,
   up() {
