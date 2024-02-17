28426869c21d9e8d67df8780b0bcdd985768f879
onlydominik <kdominikpl@gmail.com>
2024-02-17 22:25:56 +0100

    New challenge

--- index2.js
+++ index2.js
@@ -34,7 +34,7 @@ let ladder = {
   },
 };
 ladder.up().up().down().showStep().down().showStep();
-*/
+
 //challenge 34
 let ladder = {
   step: 0,
@@ -50,4 +50,67 @@ let ladder = {
   },
 };
 ladder.up().up().down().showStep().down().showStep();
+
 //NEW CHALLENGE NEW FILE
+
+function Car(brand, model, km) {
+  this.brand = brand;
+  this.model = model;
+  this.km = km;
+}
+
+let ford1 = new Car("Ford", "Mondeo", "120KM");
+
+alert(ford1.brand + ford1.model + ford1.km);
+ let cos = {};
+function A() {
+  return cos;
+}
+function B() {
+  return cos;
+}
+
+let a = new A();
+let b = new B();
+alert(new A() == new B()); // true
+alert(a == b); // true
+
+
+let Calculator = function () {
+  this.read = function () {
+    this.a = prompt("Liczba a:", 0);
+    this.b = prompt("Liczba a:", 0);
+  };
+
+  this.sum = function () {
+    return +this.a + +this.b;
+  };
+
+  this.mul = function () {
+    return this.a * this.b;
+  };
+};
+
+let calculator = new Calculator();
+calculator.read();
+
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
+*/
+
+//NEW
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    this.newValue = +prompt("Number: ", 0);
+    return (this.value += this.newValue);
+  };
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
