b2cc8f102a302fe80a8594085474e343a3e35c95
Dominik Kowalczyk <kdominikpl@gmail.com>
2024-02-27 22:49:06 +0100

    Challenge 2 27.02

--- index3.js
+++ index3.js
@@ -1,6 +1,35 @@
-let cos = {
+/*let cos = {
   test: 5,
   cos: 3,
 };
 
 alert(cos.cos);
+
+// 1
+let a = +prompt("A number: ", "");
+let b = +prompt("B number: ", "");
+
+alert(a + b);
+
+//2
+function readNumber() {
+  let num;
+
+  do {
+    num = prompt("Enter a number please?", 0);
+  } while (!isFinite(num));
+
+  if (num === null || num === "") return null;
+
+  return +num;
+}
+
+alert(`Read: ${readNumber()}`);
+*/
+//3 random numbers
+
+let newRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+alert(newRandom(5, 10));
