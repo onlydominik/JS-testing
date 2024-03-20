a7d98c39f0a78ebea4a2ca41c172fd7312adfd66
Dominik Kowalczyk <kdominikpl@gmail.com>
2024-03-20 15:56:52 +0100

    Destructuring tasks

--- index3.js
+++ index3.js
@@ -375,7 +375,7 @@ function count(obj) {
 }
 
 alert(count(user)); // 2
-*/
+
 //or short
 let user = {
   name: "John",
@@ -386,3 +386,32 @@ function count(obj) {
   return Object.entries(obj).length;
 }
 alert(count(user)); // 2
+
+
+//desctructuring
+let user = { name: "John", years: 30 };
+
+let { name, years: age, isAdmin = false } = user;
+
+alert(name); // John
+alert(age); // 30
+alert(isAdmin); // false
+*/
+//next
+let salaries = {};
+
+function topSalary(salaries) {
+  maxSalary = 0;
+  maxName = undefined;
+
+  for (let [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary;
+      maxName = name;
+    }
+  }
+
+  alert(maxName);
+}
+
+topSalary(salaries);
