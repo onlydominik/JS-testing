7dcb2ee168bef67def99a31ed551458ebb00d712
Dominik Kowalczyk <kdominikpl@gmail.com>
2024-04-05 23:56:04 +0200

    New tasks

--- index3.js
+++ index3.js
@@ -601,3 +601,21 @@ let table = document.body.firstElementChild;
 for (let i = 0; i < table.rows.length; i++) {
   table.rows[i].cells[i].style.background = "red";
 }
+
+alert(factorial(5));
+
+//DOM js children
+let div1 = document.body.firstChild;
+alert(div1);
+
+let ul1 = document.body.children[1];
+
+let li1 = document.body.children[1].children[1];
+li1.style.background = "red";
+*/
+//color table cell
+
+let table = document.body.firstElementChild;
+for (let i = 0; i < table.rows.length; i++) {
+  table.rows[i].cells[i].style.background = "red";
+}
