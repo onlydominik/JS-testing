d88cd8266213a5df1d63088a45a3030a97572436
Dominik Kowalczyk <kdominikpl@gmail.com>
2024-04-08 19:41:57 +0200

    Closure filter and sort

--- index3.js
+++ index3.js
@@ -625,7 +625,7 @@ let cos = f();
 alert(cos());
 alert(cos());
 alert(cos());
-*/
+
 
 function Counter() {
   let count = 0;
@@ -645,3 +645,53 @@ function Counter() {
 alert(Counter()); // 1
 
 alert(Counter.down()); // 1
+
+//sum with closure
+
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+alert(sum(5)(5));
+
+//filter through function
+
+function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b;
+  };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inBetween(3, 6)) );
+
+
+function inArray(arr) {
+  return function(x) {
+    return arr.includes(x);
+  };
+}
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+*/
+//sort with enclosure
+function byField(field) {
+  return function (a, b) {
+    return a[field] > b[field] ? 1 : -1;
+  };
+}
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" },
+];
+
+users.sort(byField("name"));
+users.sort(byField("age"));
+
+alert(users);
